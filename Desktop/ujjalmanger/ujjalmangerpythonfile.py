ae85fc04226d51b4cef786b4cfbdfeea444b7633
unknown <ujjal.manger@msu.edu.in>
2023-04-18 16:20:14 +0530

    python data typr

---
+++ Desktop/ujjalmanger/ujjalmangerpythonfile.py
@@ -0,0 +1,4 @@
+length = float(input('Enter the length: '))
+width = float(input('Enter the width: '))
+area = length * width
+print('The area of the rectangle is: ',area)      
---
+++ Desktop/ujjalmanger/ujjalpython.py
@@ -0,0 +1,5 @@
+pi=22/7
+radius=float(input('enter the radius :'))
+height=float(input('enter the height:'))
+volume=pi*(radius**2)*height
+print('the volume of cylinder is:',volume)
